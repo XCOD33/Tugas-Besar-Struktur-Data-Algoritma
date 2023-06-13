b883cd5f7c9bfc81878a06d6f614709e4217a099
XCOD33 <denistraju155@gmail.com>
2023-06-13 16:09:42 +0700

    add bst for insert and inorder

--- Input_Data.py
+++ Input_Data.py
@@ -1,42 +1,76 @@
 class BSTNode:
-  def __init__(self, val=None):
+  def __init__(self, barang):
     self.left = None
     self.right = None
-    self.val = val
+    self.sku = barang['sku']
+    self.nama_barang = barang['nama_barang']
+    self.harga_satuan = barang['harga_satuan']
+    self.jumlah_stok = barang['jumlah_stok']
     
-  def insert(self, val):
-    if not self.val:
-      self.val = val
+  def insert(self, barang):
+    if not self.sku:
+      self.sku = barang['sku']
+      self.nama_barang = barang['nama_barang']
+      self.harga_satuan = barang['harga_satuan']
+      self.jumlah_stok = barang['jumlah_stok']
       return
-    if self.val == val:
+    if self.sku == barang['sku']:
       return
-    if val < self.val:
+    if barang['sku'] < self.sku:
       if self.left:
-        self.left.insert(val)
+        self.left.insert(barang)
         return
-      self.left = BSTNode(val)
+      self.left = BSTNode(barang)
       return
     if self.right:
-      self.right.insert(val)
+      self.right.insert(barang)
       return
-    self.right = BSTNode(val)
+    self.right = BSTNode(barang)
     
   def get_min(self):
     current = self
     while current.left is not None:
       current = current.left
-    return current.val
+    return current.sku
     
-  def inorder(self, vals):
+  def inorder(self, barangs):
     if self.left is not None:
-      self.left.inorder(vals)
-    if self.val is not None:
-      vals.append(self.val)
+      self.left.inorder(barangs)
+    if self.sku is not None:
+      barangs.append({
+        "sku": self.sku,
+        "nama_barang": self.nama_barang,
+        "harga_satuan": self.harga_satuan,
+        "jumlah_stok": self.jumlah_stok,
+      })
     if self.right is not None:
-      self.right.inorder(vals)
-    return vals
+      self.right.inorder(barangs)
+    return barangs
   
-r = BSTNode(0, "Buku Tulis", 2000, 10)
-r.insert(28)
-r.insert(1)
+dict1 = {
+  "sku": 5,
+  "nama_barang": "Buku Tulis Sidu",
+  "harga_satuan": 2000,
+  "jumlah_stok": 10,
+}
+
+dict2 = {
+  "sku": 3,
+  "nama_barang": "Bolpoin",
+  "harga_satuan": 5000,
+  "jumlah_stok": 1,
+}
+
+dict3 = {
+  "sku": 2,
+  "nama_barang": "Penghapus",
+  "harga_satuan": 1000,
+  "jumlah_stok": 5,
+}
+  
+r = BSTNode(dict1)
+# r.insert(dict1)
+r.insert(dict2)
+r.insert(dict3)
+# r.insert(dict3)
 print(r.inorder([]))
