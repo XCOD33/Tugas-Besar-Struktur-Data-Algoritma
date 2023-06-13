376bf4d0da29f9c7762f8010fc99433b571862ef
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2023-06-13 15:21:05 +0700

    add class and method transaksi

---
+++ transaksi.py
@@ -0,0 +1,15 @@
+class Transaksi:
+    def __init__(self, nama_konsumen, no_sku, jumlah_beli, subtotal):
+        self.nama_konsumen = nama_konsumen
+        self.no_sku = no_sku
+        self.jumlah_beli = jumlah_beli
+        self.subtotal = subtotal
+
+    def inputData():
+        print('input')
+
+    def checkAllTransaction():
+        print('check')
+
+    def checkTransactionBySubtotal():
+        print('input')
