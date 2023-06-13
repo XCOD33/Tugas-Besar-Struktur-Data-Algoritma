c143bad64353e807af226aa8eed7808692feb63f
sakanusiw <saka.interisti@gmail.com>
2023-06-13 18:47:42 +0700

    fixing the codes

--- transaksi.py
+++ transaksi.py
@@ -15,18 +15,31 @@ class Transaksi:
 
         return data
 
+    def checkAllTransactions(transactions):
+        pass
+
     def checkTransactionBySubtotal(transactions):
         if len(transactions) == 0:
             print("Belum ada data transaksi")
         else:
-            for transaksi in data_transaksi:
-                nama_konsumen, sku_barang, jumlah_beli, subtotal = transaksi
-                print("Nama Konsumen:", nama_konsumen)
-                print("No. SKU Barang:", sku_barang)
-                print("Jumlah Beli:", jumlah_beli)
-                print("Subtotal:", subtotal)
-                print("---------------------------------------------------------------")
-        # return 
+            def bubble_sort(arr):
+                n = len(arr)
+                for i in range(n - 1):
+                    for j in range(n - i - 1):
+                        if arr[j].subtotal < arr[j + 1].subtotal:
+                            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            
+            def display_transaksi(transactions):
+                print("Data transaksi konsumen:")
+                print("Nama Konsumen\tNo. SKU barang\tJumlah Beli\tSubtotal")
+                for transaction in transactions:
+                    print("{}\t\t{}\t\t{}\t\t{}".format(transaction.nama_konsumen, transaction.no_sku, transaction.jumlah_beli, transaction.subtotal))
 
-    def checkTransactionBySubtotal():
-        print('input')
+            bubble_sort(transactions)
+            display_transaksi(transactions)
+
+t = Transaksi('Sugeng', 'SKU001', 5, 100000)
+t1 = Transaksi('Sogong', 'SKU002', 2, 350000)
+transactions = [t,t1]
+print(len(transactions))
+Transaksi.checkTransactionBySubtotal(transactions)
