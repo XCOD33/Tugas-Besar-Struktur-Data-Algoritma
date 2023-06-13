9e91ed22408005ce2653212e43bf3cd805895ee5
FajarWnugra <fajarwn474@gmail.com>
2023-06-13 15:39:59 +0700

    membuat fungsi checkall transaction

--- transaksi.py
+++ transaksi.py
@@ -9,7 +9,19 @@ class Transaksi:
         print('input')
 
     def checkAllTransaction():
-        print('check')
+        data_transaksi = []
+        print("=== Data Seluruh Transaksi Konsumen ===")
+        if len(data_transaksi) == 0:
+            print("Belum ada data transaksi.")
+        else:
+            for transaksi in data_transaksi:
+                nama_konsumen, sku_barang, jumlah_beli, subtotal = transaksi
+                print("Nama Konsumen:", nama_konsumen)
+                print("No. SKU Barang:", sku_barang)
+                print("Jumlah Beli:", jumlah_beli)
+                print("Subtotal:", subtotal)
+                print("---------------------------------------------------------------")
+        # return 
 
     def checkTransactionBySubtotal():
         print('input')
