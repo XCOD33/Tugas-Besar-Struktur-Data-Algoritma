b7b4b9f653512911c8faca148f7d0b26b90ee210
XCOD33 <denistraju155@gmail.com>
2023-06-14 08:16:22 +0700

    refactor code add comment on code

--- BST.py
+++ BST.py
@@ -1,11 +1,14 @@
 class BSTNode:
-  def __init__(self, barang = None):
+  # Inisialisasi node
+  def __init__(self, barang=None):
     self.left = None
     self.right = None
     self.sku = None
     self.nama_barang = None
     self.harga_satuan = None
     self.jumlah_stok = None
+
+    # Jika ada data barang, inisialisasi atribut node dengan data tersebut
     if barang:
       self.sku = barang['sku']
       self.nama_barang = barang['nama_barang']
@@ -14,92 +17,81 @@ class BSTNode:
     
   def insert(self, barang):
     if not self.sku:
+      # Jika node kosong, masukkan data barang ke dalam node
       self.sku = barang['sku']
       self.nama_barang = barang['nama_barang']
       self.harga_satuan = barang['harga_satuan']
       self.jumlah_stok = barang['jumlah_stok']
       return
+
+    # Jika SKU barang sudah ada dalam node, cetak pesan dan hentikan operasi penambahan
     if self.sku == barang['sku']:
       print("\nSKU sudah ada")
       return
+
     print("\nSKU telah dimasukkan")
+
     if barang['sku'] < self.sku:
       if self.left:
+        # Jika ada child node di sebelah kiri, rekursif panggil method insert pada child node tersebut
         self.left.insert(barang)
         return
       self.left = BSTNode(barang)
       return
+
     if self.right:
+      # Jika ada child node di sebelah kanan, rekursif panggil method insert pada child node tersebut
       self.right.insert(barang)
       return
+
     self.right = BSTNode(barang)
     
   def exists(self, sku):
     if sku == self.sku:
       return self
-    
+
     if not self.sku:
       return False
     
     if sku < self.sku:
       if self.left == None:
         return False
+      # Jika ada child node di sebelah kiri, rekursif panggil method exists pada child node tersebut
       return self.left.exists(sku)
     
     if self.right == None:
       return False
+    # Jika ada child node di sebelah kanan, rekursif panggil method exists pada child node tersebut
     return self.right.exists(sku)
   
   def restok_barang(self, sku, jumlah_stok):
     if self.exists(sku):
       barang = self.exists(sku)
       if barang:
+        # Jika SKU barang ditemukan, tambahkan jumlah stok baru ke stok barang yang ada
         barang.jumlah_stok += jumlah_stok
         print("\nBarang berhasil di restok")
         return
+
     print("\nSKU tidak ditemukan")
     return  
     
   def inorder(self, barangs):
     if self.left is not None:
+      # Rekursif panggil method inorder pada child node di sebelah kiri
       self.left.inorder(barangs)
+
     if self.sku is not None:
+      # Tambahkan data barang ke dalam list barangs
       barangs.append({
         "sku": self.sku,
         "nama_barang": self.nama_barang,
         "harga_satuan": self.harga_satuan,
         "jumlah_stok": self.jumlah_stok,
       })
-    if self.right is not None:
-      self.right.inorder(barangs)
-    return barangs
-  
-# dict1 = {
-#   "sku": 0000,
-#   "nama_barang": "AAA",
-#   "harga_satuan": 1000,
-#   "jumlah_stok": 10,
-# }
-# dict2 = {
-#   "sku": 1111,
-#   "nama_barang": "BBBB",
-#   "harga_satuan": 2000,
-#   "jumlah_stok": 20,
-# }
-# dict3 = {
-#   "sku": 22222,
-#   "nama_barang": "CCC",
-#   "harga_satuan": 3000,
-#   "jumlah_stok": 30,
-# }
-# dict4 = {
-#   "sku": 1111,
-#   "nama_barang": "DDD",
-#   "harga_satuan": 4000,
-#   "jumlah_stok": 40,
-# }
 
-# r = BSTNode(dict1)
-# r.insert(dict2)
-# r.insert(dict3)
-# print(r.exists(1111))
+    if self.right is not None:
+      # Rekursif panggil method inorder pada child node di sebelah kanan
+      self.right.inorder(barangs)
+
+    return barangs
--- tugas-besar.py
+++ tugas-besar.py
@@ -2,121 +2,126 @@ from BST import BSTNode
 from transaksi import Transaksi
 import pandas as pd
 
-bst = BSTNode()
+bst = BSTNode()  # Membuat instance objek dari kelas BSTNode
 
-transaksi = Transaksi({
+transaksi = Transaksi({  # Membuat instance objek dari kelas Transaksi
     "nama_konsumen": "",
     "no_sku": 0000,
     "jumlah_beli": 0,
     "subtotal": 0
 })
 
-answer = True
-dataTransaksi = []
+answer = True  # Variabel untuk menjaga agar program tetap berjalan
+dataTransaksi = []  # List untuk menyimpan data transaksi
 
 while answer:
     print("Selamat Datang di Aplikasi Kelola Barang\n1) Kelola Stok Barang\n2) Kelola Transaksi Konsumen\n0) Keluar\n")
 
-    input_menu = input("Masukkan Menu : ")
+    input_menu = input("Masukkan Menu : ")  # Mengambil input menu dari pengguna
 
-    if (input_menu == "1"):
-        
-        sub_answer_1 = True
-        
+    if input_menu == "1":  # Jika menu yang dipilih adalah 1 (Kelola Stok Barang)
+        sub_answer_1 = True  # Variabel untuk menjaga agar tetap berada di dalam sub-menu 1
+
         while sub_answer_1:
             print("\nKelola Stok Barang\n1) Input Data Stok Barang\n2) Restok Barang\n3) Daftar Barang\n0) Keluar\n")
-            
-            sub_menu_1 = input("Masukkan Menu : ")
-            
-            if(sub_menu_1 == "1"):
+
+            sub_menu_1 = input("Masukkan Menu : ")  # Mengambil input sub-menu 1 dari pengguna
+
+            if sub_menu_1 == "1":  # Jika sub-menu yang dipilih adalah 1 (Input Data Stok Barang)
                 print("\n=== Input Data Stok Barang ===\n")
-                
-                input_sku = input("Masukkan SKU Barang : ")
-                
-                if len(str(input_sku)) < 4 or len(str(input_sku)) > 4:
+
+                input_sku = input("Masukkan SKU Barang : ")  # Mengambil input SKU barang dari pengguna
+
+                if len(str(input_sku)) < 4 or len(str(input_sku)) > 4:  # Validasi panjang SKU harus 4 digit
                     print("\nSKU harus 4 digit")
                     continue
-                if bst.exists(input_sku) == True:
+                if bst.exists(input_sku) == True:  # Validasi SKU barang sudah ada
                     print("\nSKU sudah ada")
                     continue
-                
-                input_nama_barang = input("Masukkan Nama Barang : ")
-                input_harga_satuan = int(input("Masukkan Harga Barang : "))
-                input_jumlah_stok = int(input("Masukkan Jumlah Stok : "))
-                
-                bst.insert({
+
+                input_nama_barang = input("Masukkan Nama Barang : ")  # Mengambil input nama barang dari pengguna
+                input_harga_satuan = int(input("Masukkan Harga Barang : "))  # Mengambil input harga satuan dari pengguna
+                input_jumlah_stok = int(input("Masukkan Jumlah Stok : "))  # Mengambil input jumlah stok dari pengguna
+
+                bst.insert({  # Menyisipkan data barang ke dalam BST
                     "sku": input_sku,
                     "nama_barang": input_nama_barang,
                     "harga_satuan": input_harga_satuan,
                     "jumlah_stok": input_jumlah_stok,
                 })
-                print(bst.inorder([]))
-                
-            elif(sub_menu_1 == "2"):
+                print(bst.inorder([]))  # Menampilkan daftar barang menggunakan traversal inorder BST
+
+            elif sub_menu_1 == "2":  # Jika sub-menu yang dipilih adalah 2 (Restok Barang)
                 print("\n=== Restok Barang ===\n")
-                input_sku = input("Masukkan SKU Barang : ")
-                if bst.exists(input_sku) == False:
+                input_sku = input("Masukkan SKU Barang : ")  # Mengambil input SKU barang yang akan direstok
+
+                if bst.exists(input_sku) == False:  # Validasi SKU barang tidak ditemukan
                     print("\nSKU tidak ditemukan")
                     continue
-                input_jumlah_stok = int(input("Masukkan Jumlah Stok : "))
-                bst.restok_barang(input_sku, input_jumlah_stok)
-                
-            elif(sub_menu_1 == "3"):
+
+                input_jumlah_stok = int(input("Masukkan Jumlah Stok : "))  # Mengambil input jumlah stok yang akan ditambahkan
+                bst.restok_barang(input_sku, input_jumlah_stok)  # Melakukan restok barang
+
+            elif sub_menu_1 == "3":  # Jika sub-menu yang dipilih adalah 3 (Daftar Barang)
                 df = pd.DataFrame(bst.inorder([]), columns=['sku', 'nama_barang', 'harga_satuan', 'jumlah_stok'])
-                print(f"\nDaftar Transaksi\n{df}\n")
-                
-            elif(sub_menu_1 == "0"):
+                print(f"\nDaftar Transaksi\n{df}\n")  # Menampilkan daftar barang menggunakan Pandas DataFrame
+
+            elif sub_menu_1 == "0":  # Jika sub-menu yang dipilih adalah 0 (Keluar)
                 print("\nKeluar\n")
-                sub_answer_1 = False
-                
+                sub_answer_1 = False  # Keluar dari sub-menu 1
+
             else:
                 print("\nPilihan tidak tersedia\n")
-                
-    elif input_menu == "2":
-        sub_answer_2 = True
+
+    elif input_menu == "2":  # Jika menu yang dipilih adalah 2 (Kelola Transaksi Konsumen)
+        sub_answer_2 = True  # Variabel untuk menjaga agar tetap berada di dalam sub-menu 2
 
         while sub_answer_2:
             print("\nKelola Transaksi\n1) Input Data Transaksi\n2) Lihat Data Seluruh Transaksi Konsumen\n3) Lihat Data Transaksi Berdasarkan Subtotal\n0) Keluar\n")
 
-            sub_menu_2 = input("Masukkan Menu : ")
+            sub_menu_2 = input("Masukkan Menu : ")  # Mengambil input sub-menu 2 dari pengguna
 
-            if sub_menu_2 == "1":
+            if sub_menu_2 == "1":  # Jika sub-menu yang dipilih adalah 1 (Input Data Transaksi)
                 print("\n=== Input Data Transaksi ===\n")
 
-                input_nama_konsumen = input("Masukkan nama konsumen = ")
-                
+                input_nama_konsumen = input("Masukkan nama konsumen = ")  # Mengambil input nama konsumen dari pengguna
+
                 input_sku_answer = True
                 while input_sku_answer:
                     while input_sku_answer:
-                        input_sku = input("Masukkan SKU Barang : ")
-                        if bst.exists(input_sku) == False:
+                        input_sku = input("Masukkan SKU Barang : ")  # Mengambil input SKU barang dari pengguna
+
+                        if bst.exists(input_sku) == False:  # Validasi SKU barang belum terdaftar
                             print("\nNo. SKU yang diinputkan belum terdaftar")
                             input_lanjut_transaksi = input("Apakah ingin melanjutkan transaksi (Y/N)? : ")
                             if input_lanjut_transaksi == "Y" or input_lanjut_transaksi == "y":
                                 continue
                             break
                         break
+
                     input_jumlah_beli_answer = True
                     while input_jumlah_beli_answer:
-                        input_jumlah_beli = int(input("Masukkan jumlah beli = "))
+                        input_jumlah_beli = int(input("Masukkan jumlah beli = "))  # Mengambil input jumlah beli dari pengguna
                         barang = bst.exists(input_sku)
-                        if barang.jumlah_stok < input_jumlah_beli:
+
+                        if barang.jumlah_stok < input_jumlah_beli:  # Validasi jumlah stok barang tidak mencukupi
                             print("Jumlah Stok No.SKU yang Anda beli tidak mencukupi")
                             input_lanjut_transaksi = input("Apakah ingin melanjutkan transaksi (Y/N)? : ")
                             if input_lanjut_transaksi == "Y" or input_lanjut_transaksi == "y":
                                 continue
                             break
-                        barang.jumlah_stok -= input_jumlah_beli
-                        subtotal = input_jumlah_beli * barang.harga_satuan
 
-                        transaksi.inputData({
+                        barang.jumlah_stok -= input_jumlah_beli  # Mengurangi jumlah stok barang yang dibeli
+                        subtotal = input_jumlah_beli * barang.harga_satuan  # Menghitung subtotal transaksi
+
+                        transaksi.inputData({  # Memasukkan data transaksi ke dalam objek Transaksi
                             "nama_konsumen": input_nama_konsumen,
                             "no_sku": input_sku,
                             "jumlah_beli": input_jumlah_beli,
                             "subtotal": subtotal
                         })
 
-                        dataTransaksi.append({
+                        dataTransaksi.append({  # Menyimpan data transaksi ke dalam list
                             "nama_konsumen": input_nama_konsumen,
                             "no_sku": input_sku,
                             "jumlah_beli": input_jumlah_beli,
@@ -126,32 +131,31 @@ while answer:
                         print('\nData Transaksi Konsumen Berhasil Diinputkan\n')
                         print(dataTransaksi)
                         break
+
                     input_tambah_beli = input('Apakah ingin menambahkan data pembelian untuk konsumen ini (Y/N)? : ')
                     if input_tambah_beli == "Y" or input_tambah_beli == "y":
                         continue
                     break
 
-            elif sub_menu_2 == "2":
+            elif sub_menu_2 == "2":  # Jika sub-menu yang dipilih adalah 2 (Lihat Data Seluruh Transaksi Konsumen)
                 print("\n=== Lihat Data Transaksi ===\n")
+                transaksi.checkAllTransactions(dataTransaksi)  # Menampilkan semua data transaksi
 
-                transaksi.checkAllTransactions(dataTransaksi)
-            
-            elif sub_menu_2 == "3":
+            elif sub_menu_2 == "3":  # Jika sub-menu yang dipilih adalah 3 (Lihat Data Transaksi Berdasarkan Subtotal)
                 print("\n=== Data Transaksi Berdasarkan Subtotal===\n")
+                transaksi.checkTransactionBySubtotal(dataTransaksi)  # Menampilkan data transaksi berdasarkan subtotal
 
-                transaksi.checkTransactionBySubtotal(dataTransaksi)
-                
-            elif sub_menu_2 == "0":
+            elif sub_menu_2 == "0":  # Jika sub-menu yang dipilih adalah 0 (Keluar)
                 print("\nKeluar")
-                sub_answer_2 = False
+                sub_answer_2 = False  # Keluar dari sub-menu 2
 
             else:
                 print("Menu tidak tersedia")
                 continue
-        
-    elif input_menu == "0":
+
+    elif input_menu == "0":  # Jika menu yang dipilih adalah 0 (Keluar)
         print("\nKeluar")
-        answer = False
-        
+        answer = False  # Keluar dari program
+
     else:
         print("\nPilihan tidak tersedia")
